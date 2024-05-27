8a77926b43ce1fc5dbef476cf2522a14c9cbc135
Dayvid0063 <orjidavid63@gmail.com>
2024-05-27 20:28:28 +0100

    new updates

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,12 +1,9 @@
 export default function createIteratorObject(report) {
-
-  function* empIT() {
+  return (function* _() {
     for (const department of Object.values(report.allEmployees)) {
       for (const employee of department) {
         yield employee;
       }
     }
-  }
-
-  return empIT();
+  }());
 }
