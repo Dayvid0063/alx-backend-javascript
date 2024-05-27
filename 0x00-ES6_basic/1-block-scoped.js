fe5eb218b2eb45ecf749db033da29128b8523196
Dayvid0063 <orjidavid63@gmail.com>
2024-05-27 15:52:31 +0100

    block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const task = true; // block-scoped
+    const task2 = false; // block-scoped
   }
 
   return [task, task2];
