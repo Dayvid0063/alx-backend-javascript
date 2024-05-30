1b53db1b5b67cb0814739b6bbb3f4d0a9a446341
Dayvid0063 <orjidavid63@gmail.com>
2024-05-30 17:13:48 +0100

    task 7 new

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -4,8 +4,24 @@ class Airport {
     this.code = code;
   }
 
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
   // Override toString to return the airport code
-  get [Symbol.toStringTag]() {
+  toString() {
     return this._code;
   }
 }
