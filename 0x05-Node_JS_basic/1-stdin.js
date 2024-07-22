2b17979eb2de283b16e3b4fc2b2270b899edb838
Dayvid0063 <orjidavid63@gmail.com>
2024-07-22 18:14:21 +0100

    stdin fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,11 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('data', () => {
+  const info = process.stdin.read();
 
-process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
+  if (info) {
+    process.stdout.write(`Your name is: ${info}`);
+  }
+});
+process.stdin.on('close', () => {
   process.stdout.write('This important software is now closing\n');
-  process.exit();
 });
